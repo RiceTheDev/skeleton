cb5e0b0f2810ceda13e87971926040990232bb39
RiceTheDev <ricenoob@toke.com>
2024-03-12 23:52:58 -0300

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ let totalTime = 0; // Variável para armazenar o tempo total
 let clientCount = 0; // Contador de clientes conectados
 
 // Atualiza o tempo total e envia para todos os clientes a cada segundo
-setInterval(() => {
+const timerId = setInterval(() => {
   if (clientCount > 0) { // Verifica se há pelo menos um cliente conectado
     totalTime++; // Incrementa o tempo total
     io.emit('updateTotalTime', totalTime); // Enviamos o tempo total para todos os clientes
@@ -40,4 +40,10 @@ const PORT = process.env.PORT || 3000;
 // Inicia o servidor para ouvir conexões na porta configurada
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+});
+
+// Captura o evento beforeExit para encerrar o servidor corretamente
+process.on('beforeExit', () => {
+  clearInterval(timerId); // Para o intervalo que atualiza o tempo total
+  server.close(); // Encerra o servidor corretamente
 });
