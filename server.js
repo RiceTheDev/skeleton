37aae5b82c7bac86e3f7298209a086790bc773e3
RiceTheDev <151386744+RiceTheDev@users.noreply.github.com>
2024-04-20 19:42:08 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,42 @@
-const express = require('express');
+// server.js
 const http = require('http');
-const socketIo = require('socket.io');
+const fs = require('fs');
 const path = require('path');
+const socketIo = require('socket.io');
 
-const app = express();
-const server = http.createServer(app);
+const server = http.createServer((req, res) => {
+  if (req.url === '/' && req.method === 'GET') {
+    // Serve a página HTML quando a rota raiz é acessada
+    fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
+      if (err) {
+        res.writeHead(500);
+        res.end('Internal Server Error');
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(data);
+      }
+    });
+  } else if (req.url === '/data' && req.method === 'GET') {
+    // Retorna os dados necessários para a página HTML quando a rota /data é acessada
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ totalTime, clientCount }));
+  } else {
+    // Se a rota não for encontrada, retorna um erro 404
+    res.writeHead(404);
+    res.end('Not Found');
+  }
+});
+
 const io = socketIo(server);
 
-let totalTime = 0; // Variável para armazenar o tempo total
-let clientCount = 0; // Contador de clientes conectados
+let totalTime = 0; // Tempo total
+let clientCount = 0; // Contador de clientes
 
 // Atualiza o tempo total e envia para todos os clientes a cada segundo
 const timerId = setInterval(() => {
-  if (clientCount > 0) { // Verifica se há pelo menos um cliente conectado
-    totalTime++; // Incrementa o tempo total
-    io.emit('updateTotalTime', totalTime); // Enviamos o tempo total para todos os clientes
+  if (clientCount > 0) {
+    totalTime++;
+    io.emit('updateTotalTime', totalTime);
   }
 }, 1000);
 
@@ -34,23 +56,8 @@ io.on('connection', (socket) => {
   });
 });
 
-// Define o diretório raiz para servir arquivos estáticos
-const rootDir = path.join(__dirname, '/');
-app.use(express.static(rootDir));
-
-// Verifica se estamos em um ambiente de desenvolvimento
-if (process.env.NODE_ENV !== 'production') {
-  // Inicia o servidor para ouvir conexões na porta 3000
-  const PORT = process.env.PORT || 3000;
-  server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-}
-
-// Captura o evento beforeExit para encerrar o servidor corretamente
-process.on('beforeExit', () => {
-  clearInterval(timerId); // Para o intervalo que atualiza o tempo total
+// Inicia o servidor
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
-
-// Exporta o app para que possa ser usado em outros arquivos (opcional)
-module.exports = app;
